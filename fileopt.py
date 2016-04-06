42c5c5f97c227a0da3a41de2e30202af1a7849b1
MiCooLvy <canceman88@gmail.com>
2016-04-06 23:47:32 +0800

    add files

---
+++ fileopt.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+import curses, time
+
+
+def writef():
+    fp = open("datas.txt", 'w')
+    for i in range(10000000):
+        fp.write("ID:" + str(i) + '\n')
+    fp.close()
+
+
+def readf():
+    fp = open("datas.txt", "r")
+    scr = curses.initscr()
+    scr.border(0)
+    line = fp.readline()
+    time.clock()
+    while line != "":
+        line = fp.readline()
+        scr.refresh()
+    tm = time.clock()
+    scr.addstr(10, 7, str(tm))
+    scr.getch()
+    fp.close()
+    curses.endwin()
+
+
+def main():
+    try:
+        readf()
+    except IOError:
+        writef()
+
+
+if __name__ == '__main__':
+    main()
